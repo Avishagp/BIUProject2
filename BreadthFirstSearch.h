76dba33f5cbb9ce8f51e9b476acb30d9209b9f1a
Avishagp <44115709+Avishagp@users.noreply.github.com>
2019-01-13 20:28:23 +0200

    Made changes to DFS and BFS to make more sense

--- BreadthFirstSearch.h
+++ BreadthFirstSearch.h
@@ -10,10 +10,42 @@
 template <class P, class S>
 
 class BreadthFirstSearch : public ISearcher<P,S> {
-private:
-    S searchInAllPossibleStates(ISearchable<P> searchable, State<S> *initial, State<S> *goal);
 public:
-    S search(ISearchable<P> searchable) override;
+    S search(ISearchable<P>* searchable) override {
+        /* Get the initial state and the goal state from the searchable. */
+        State<S> *initial = searchable->getInitialState();
+        State<S> *goal = searchable->getGoalState();
+
+        std::queue<State<S>> qs;
+        qs.push(initial);
+        initial->setVisited(true);
+        State<S> node;
+
+        /* Loop on every node. */
+        while (!qs.empty()) {
+            node = qs.front();
+            qs.pop(); //delete the element we have just gotten.
+            this->numberOfNodesEvaluated++;
+
+            if (node.Equals(goal)) {
+                return goal->getState();
+            }
+
+            /* Iterate on all of node's children. */
+            std::vector<State<S>> possibles = searchable->getAllPossibleStates(node);
+            typename std::vector<State<S>>::iterator itor;
+
+            for (itor = possibles.begin(); itor != possibles.end(); itor++) {
+                /* If a child node wasn't visited, add to queue. */
+                if (!itor->getVisited()) {
+                    itor->setVisited(true);
+                    qs.push(itor);
+                    itor->setCameFrom(node);
+                }
+            }
+        }
+        return node.getState(); //all sons weren't goal.
+    }
 };
 
 
--- DepthFirstSearch.h
+++ DepthFirstSearch.h
@@ -10,10 +10,38 @@
 template <class P, class S>
 
 class DepthFirstSearch : public ISearcher<P,S> {
-private:
-    S searchInPossibleStates(ISearchable<P> searchable, State<S> *current, State<S> *goal);
 public:
-    S search(ISearchable<P> searchable) override;
+    S search(ISearchable<P>* searchable) override {
+        /* Get the initial state and the goal state from the searchable. */
+        State<S> *current = searchable->getInitialState();
+        State<S> *goal = searchable->getGoalState();
+
+        typename std::vector<State<S>>::iterator itor;
+        std::vector<State<S>> possible = searchable->getAllPossibleStates(current);
+
+        /* Iterate on all the possible states. */
+        for (itor = possible.begin(); itor != possible.end(); itor++) {
+            /* Set the node that we came from. */
+            itor->setCameFrom(current);
+
+            if (itor->getVisited()) { //node has already been visited.
+                continue;
+            } else {
+                itor->setState(true); //set visited
+                this->numberOfNodesEvaluated++; //increment number of nodes visited
+
+                if (itor->Equals(goal)) { //If current state is the goal, return.
+                    return goal->getState();
+                } else {
+                    /* Search all sons of itor. If one of them is goal, return it! */
+                    if (searchInPossibleStates(searchable, &itor, goal) == goal->getState()) {
+                        return goal->getState();
+                    }
+                }
+            }
+        }
+        return current->getState(); //all sons weren't goal
+    }
 };
 
 
